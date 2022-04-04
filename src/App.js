7b26f727381ce4ff96045dea091eeef39c2a0b7f
Lucas <lcs.sc95@gmail.com>
2022-04-03 21:14:34 -0300

    ajustes gerais

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import {useState} from 'react';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App (){
+
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState('');
+
+  function addItem(){
+    if(input !== ''){
+      var item = list.push = input;
+      setList(list => [...list, item]);
+      setInput('');
+    }else{
+      alert("Digite uma tarefa para ser inserida!");
+    }
+  }
+
+    return (
+      <div className="container">
+        <h1 id="title">LISTA DE TAREFAS</h1>
+
+        <div className="newTask">
+          <input type="text" id="inputTask" placeholder="Digite uma nova tarefa..." value={input} onChange={(event) => setInput(event.target.value)}/>
+          <button id="addTask" onClick={addItem}>+</button>
+        </div>    
+
+        {list.map((item, indice) => (
+        <div className="itemList" key={indice}>
+          <span>{item}</span>
+        </div>
+        ))}
+
+      </div>
+    );
+};
 
 export default App;
